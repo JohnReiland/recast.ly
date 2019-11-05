bdef17e2ce79f614a7418b8fdfd153f9639765fd
JohnReiland <reiland.shanghai@gmail.com>
2019-11-05 10:50:55 -0500

    "Initial Commit -- Add page load"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,9 @@ var App = () => (
   </div>
 );
 
+export default App;
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-export default App;
+
+
