2062938b926e9ad4756368c12b7fd5a6a2d8e379
JohnReiland <reiland.shanghai@gmail.com>
2019-11-05 14:08:04 -0500

    "Complete VideoList refactor"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,15 +10,15 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><VideoList videos={exampleVideoData}/></div>
       </div>
     </div>
   </div>
 );
 
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
 export default App;
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-
-
